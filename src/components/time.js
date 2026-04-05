8dbf0e5a55b2f38d3b403b86255a3e6baed9c113
Aghnia Prawira <aghniaputriprawira@gmail.com>
2026-04-05 19:50:14 +0100

    Add dotted manga background.

--- src/components/time.js
+++ src/components/time.js
@@ -1,16 +1,52 @@
+"use client";
+
 import { HStack, Image, Stack, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 export default function Time() {
+  const [time, setTime] = useState({
+    hour: "",
+    minute: "",
+    period: "",
+  });
+
+  useEffect(() => {
+    const update = () => {
+      const parts = new Intl.DateTimeFormat("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true,
+      }).formatToParts(new Date());
+
+      setTime({
+        hour: parts.find((p) => p.type === "hour")?.value || "",
+        minute: parts.find((p) => p.type === "minute")?.value || "",
+        period: parts.find((p) => p.type === "dayPeriod")?.value || "",
+      });
+    };
+
+    update();
+    const interval = setInterval(update, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <Stack bgColor="white" padding={0} width="172px" gap={0}>
+    <Stack
+      bgColor="white"
+      padding={0}
+      width="172px"
+      gap={0}
+      border="2px solid black"
+      className={"manga-dots border border-black p-4"}
+    >
       <Stack padding={4} gap={0} paddingBottom={2} paddingRight={2}>
         <Text textAlign="right" fontWeight={"bold"} fontSize={"3xl"}>
-          12:24
+          {time.hour}:{time.minute}
         </Text>
         <HStack alignItems="right" justifyContent="right">
           <Image src={"/images/moon.png"} height={"25px"} width={"25px"} />
           <Text textAlign="right" fontWeight={"bold"} fontSize={"xl"}>
-            AM
+            {time.period}
           </Text>
         </HStack>
       </Stack>
